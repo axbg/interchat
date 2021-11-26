c834fdb8dfe2786a6fe53f16f5c21dcbe89bc09f
Stefan <lazeastefan@gmail.com>
2021-11-26 16:23:54 +0200

    Init structure

---
+++ front/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { Speech } from './components/Speech/Speech'
+import Button from '@mui/material/Button';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Button variant="contained">Hello World</Button>
+      <Speech />
+
+    </div>
+  );
+}
+
+export default App;
