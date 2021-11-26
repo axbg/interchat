d233d36710432e024621d02cc99e4eeff1a6b1e6
Stefan <lazeastefan@gmail.com>
2021-11-26 16:50:28 +0200

    Add scss

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { Speech } from './components/Speech/Speech'
 import Button from '@mui/material/Button';
 
---
+++ front/src/pages/explore-rooms/ExploreRooms.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { CustomCard } from '../../components/common/CustomCard'
+
+import './ExploreRooms.scss'
+
+export const ExploreRooms = () => {
+    const bull = (
+        <Box
+            component="span"
+            sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+        >
+            •
+        </Box>
+    );
+    return (
+        <Container fixed>
+            <div className={'explore-rooms'}>
+                <Card>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                            Word of the Day
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                            be{bull}nev{bull}o{bull}lent
+                        </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                            adjective
+                        </Typography>
+                        <Typography variant="body2">
+                            well meaning and kindly.
+                            <br />
+                            {'"a benevolent smile"'}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+                <CustomCard />
+            </div>
+        </Container>
+    )
+}
